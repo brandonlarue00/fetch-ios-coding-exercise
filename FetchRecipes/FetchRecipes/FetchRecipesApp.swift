22e777525552a5cea5c65305a6c3fe39d99d9f87
Brandon <brandonlarue00@gmail.com>
2024-06-14 16:57:52 -0400

    Started XCode Project in repo Changes Cloned repo to local machine then started an xcode project in repo. Nothing else has been done so far this is the initial commit to remote.
    Future Directions
    Going to create a dev branch and do all of my work there which will be merged to main before submission. Next step will being setting up project architecture using MVVM with services and utilities directories. I will likely rename the ContentView file to something more relevant and then move into a /Views directory. After that I will set up the service to fetch the recipes from api and log so I can create a model once I see what all I can get from the first endpoint.
    
    Note
    N/A

---
+++ FetchRecipes/FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Brandon LaRue on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
